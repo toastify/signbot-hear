bd0ed72c3382ad7709ff1c0c56956083dd063927
Clive Chan <cc@clive.io>
2017-01-29 08:28:26 -0500

    this is the sketchiest thing ever

---
+++ html/configuration.js
@@ -0,0 +1,17 @@
+// Credentials
+
+var URL = 'wss://ws.dev.nuance.com/?';
+
+var APP_ID = "NMDPTRIAL_cc_clive_io20170128210627";
+var APP_KEY = "1240d813902d8efd33fff5690dd09cd06baa6af3b81c51177913e0ce501ee2625eba75eee804eb1b03e08411b021e11bcb4c0f8368609ae1737bafc3999125f7";
+var USER_ID = "";
+var NLU_TAG = "coffee";
+
+// ASR
+// See: https://developer.nuance.com/public/index.php?task=supportedLanguages
+var ASR_LANGUAGE = "eng-USA";
+
+// TTS
+// See: https://developer.nuance.com/public/index.php?task=supportedLanguages
+var TTS_LANGUAGE = "eng-USA";
+var TTS_VOICE = "";
--- index.js
+++ index.js
@@ -1,14 +1,62 @@
-let Nuance = require('nuance');
 let child_process = require('child_process');
 let samples = require('../signbot-data/data').samples;
+let request = require('request');
+let randomstring = require('randomstring');
 
 require('dotenv-safe').load();
 
 let talker = child_process.fork('../signbot-talk/index');
 
-//stream to Nuance
+var fs = require('fs');
 
-on("word", function(word){
-  if(samples[word])
-    talker.send(samples[word]);
+let qs = require('querystring').stringify({
+  appId: process.env.NUANCE_APP_ID,
+  appKey: process.env.NUANCE_APP_KEY,
+  id: randomstring.generate(10)
+});
+
+let queue = [];
+let interval;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+let app = express();
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/html'));
+app.all('/talk', (req, res) => {
+  if(req.body.transcriptions){
+    console.log(req.body.transcriptions[0]);
+    queue[queue.length] = req.body.transcriptions[0];
+    if(!interval)
+      interval = setInterval(function(){
+        if(queue.length){
+          let to_say = queue.shift();
+          while(!samples[to_say] && queue.length)
+            to_say = queue.shift();
+          if(samples[to_say])
+            talker.send(to_say.toLowerCase());
+        }else{
+          talker.send('null');
+          clearInterval(interval);
+          interval = undefined;
+        }
+      }, 1500);
+  }
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8022');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  res.sendStatus(200);
+});
+app.listen(8022, function(){
+  console.log('listening');
 });
